cc01551f2a291964f8590cea4263a2409eecd975
JakobDev <jakobdev@gmx.de>
2022-05-25 17:54:30 +0200

    Add source

---
+++ jdDiff/translations/jdDiff_de.ts
@@ -0,0 +1,281 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../AboutDialog.ui" line="0"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../AboutDialog.ui" line="0"/>
+        <source>A graphical cross platform diff viewer</source>
+        <translation>Ein grafisches Programm zum betrachten von Dateiunterschieden</translation>
+    </message>
+    <message>
+        <location filename="../AboutDialog.ui" line="0"/>
+        <source>This Program is licensed under GPL 3</source>
+        <translation>Dieses Programm ist unter der GPL 3 lizenziert</translation>
+    </message>
+    <message>
+        <location filename="../AboutDialog.ui" line="0"/>
+        <source>View source</source>
+        <translation>Quelltext anzeigen</translation>
+    </message>
+    <message>
+        <location filename="../AboutDialog.ui" line="0"/>
+        <source>Close</source>
+        <translation>Schließen</translation>
+    </message>
+</context>
+<context>
+    <name>BrowseDialog</name>
+    <message>
+        <location filename="../BrowseDialog.py" line="72"/>
+        <source>No Original</source>
+        <translation>Kein Original</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="72"/>
+        <source>You have not set a Original path</source>
+        <translation>Es wurde kein Originalpfad angegeben</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="75"/>
+        <source>No Copy</source>
+        <translation>keine Kopie</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="75"/>
+        <source>You have not set a Copy path</source>
+        <translation>Es wurde kein Kopiepfad angegeben</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="79"/>
+        <source>Same Paths</source>
+        <translation>Gleiche Pfade</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="79"/>
+        <source>Original and Copy have the same Paths</source>
+        <translation>Original und Kopie haben die selben Pfade</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="85"/>
+        <source>Not a File</source>
+        <translation>Keine Datei</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="85"/>
+        <source>{{path}} is not a File</source>
+        <translation>{{path}} ist keine Datei</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="90"/>
+        <source>Not a Directory</source>
+        <translation>Keihn Verzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="90"/>
+        <source>{{path}} is not a Directory</source>
+        <translation>{{path}} ist kein Verzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="101"/>
+        <source>Select Files</source>
+        <translation>Dateien auswählen</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.py" line="117"/>
+        <source>Select Directories</source>
+        <translation>Verzeichnisse auswählen</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.ui" line="0"/>
+        <location filename="../BrowseDialog.ui" line="0"/>
+        <source>Browse</source>
+        <translation>Durchsuchen</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.ui" line="0"/>
+        <source>Original:</source>
+        <translation>Original:</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.ui" line="0"/>
+        <source>Copy:</source>
+        <translation>Kopie:</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.ui" line="0"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../BrowseDialog.ui" line="0"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>CompareThread</name>
+    <message>
+        <location filename="../CompareThread.py" line="40"/>
+        <location filename="../CompareThread.py" line="39"/>
+        <source>Can&apos;t show diff for binary file</source>
+        <translation>Der Unterschied zwischen Binärdateien kann nicht angezeigt werden</translation>
+    </message>
+    <message>
+        <location filename="../CompareThread.py" line="99"/>
+        <location filename="../CompareThread.py" line="61"/>
+        <location filename="../CompareThread.py" line="42"/>
+        <source>Same</source>
+        <translation>Gleich</translation>
+    </message>
+    <message>
+        <location filename="../CompareThread.py" line="95"/>
+        <location filename="../CompareThread.py" line="58"/>
+        <location filename="../CompareThread.py" line="45"/>
+        <source>Modified</source>
+        <translation>Verschieden</translation>
+    </message>
+    <message>
+        <location filename="../CompareThread.py" line="71"/>
+        <source>Not in Copy</source>
+        <translation>Nicht in der Kopie</translation>
+    </message>
+    <message>
+        <location filename="../CompareThread.py" line="88"/>
+        <source>Not in Original</source>
+        <translation>Nicht im Original</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>Path</source>
+        <translation>Pfad</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>?</source>
+        <translation>?</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>Compare Directories</source>
+        <translation>Verzeichnisse vergleichen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>About Qt</source>
+        <translation>Über Qt</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>Compare Files</source>
+        <translation>Dateien vergleichen</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>Exit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.ui" line="0"/>
+        <source>Reload</source>
+        <translation>Neuladen</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../SettingsDialog.py" line="16"/>
+        <source>System language</source>
+        <translation>Systemsprache</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.py" line="22"/>
+        <source>Alphabetically ascending</source>
+        <translation>Alphabetisch aufsteigend</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.py" line="23"/>
+        <source>Alphabetically descending</source>
+        <translation>Alphabetisch absteigend</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.py" line="24"/>
+        <source>Natural ascending</source>
+        <translation>Natürlich aufsteigend</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.py" line="25"/>
+        <source>Natural descending</source>
+        <translation>Natürlich absteigend</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="0"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="0"/>
+        <source>Language:</source>
+        <translation>Sprache:</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="0"/>
+        <source>Addded Color:</source>
+        <translation>Farbe hinzugefügt:</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="0"/>
+        <source>Removed Color:</source>
+        <translation>Farbe entfernt:</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="0"/>
+        <source>Sorting:</source>
+        <translation>Sortierung:</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="0"/>
+        <source>Reset</source>
+        <translation>Zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="0"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="0"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+</TS>
